02f4ab995a0515c5a00c07d1b02badbd8ac0e660
caizhaoke <caizhaoke@idwzx.com>
2018-02-13 09:24:10 +0800

    INIT:初始化eureka-maven父工程以及eurekaserver服务注册中心module工程和eurekaclient服务提供者module工程

---
+++ eureka/eurekaclient/src/main/java/cn/caizhaoke/EurekaclientApplication.java
@@ -0,0 +1,12 @@
+package cn.caizhaoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaclientApplication.class, args);
+	}
+}
